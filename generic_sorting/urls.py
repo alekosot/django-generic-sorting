643707d19510a3a51f78568185627deedeb4e7db
Alex Sotiriou <a.sotiriou@difio.gr>
2018-03-22 17:56:39 +0200

    Added app_name in app's urls (compatibility with django 2)

--- generic_sorting/urls.py
+++ generic_sorting/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from .views import generic_sorting
 
 
+app_name = 'generic_sorting'
+
+
 urlpatterns = [
     url(r'(?P<content_type_pk>[\w]+)/(?P<order_by_field>[\w]+)/(?P<primary_keys>[\w,]+)/',
         generic_sorting,
